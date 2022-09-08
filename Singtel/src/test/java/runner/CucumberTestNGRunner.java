0aacf0c170926816ceaac2beb892c6143cb09384
Mahendra Kishore <mahendrakishore3@gmail.com>
2022-09-09 02:44:08 +0800

    final all working test cases

--- Singtel/src/test/java/runner/CucumberTestNGRunner.java
+++ Singtel/src/test/java/runner/CucumberTestNGRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-@CucumberOptions(tags = "@deleteAllToDoListTogether", features = "src/test/resources/features/create-todos.feature", 
+@CucumberOptions(tags = "@toDo", features = "src/test/resources/features/create-todos.feature", 
 glue = "stepdefination",monochrome=true,publish=true)
 
 public class CucumberTestNGRunner extends AbstractTestNGCucumberTests{
